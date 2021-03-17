76ab933f47fc3d9cdbd84f52e8414bfb7de26f5f
yatsun <yatsunlam25@gmail.com>
2021-03-16 20:56:58 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require 'pry'
+class Song
+
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @@count += 1
+        @genre = genre
+        @artist = artist   
+        @name = name
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end   
+    
+    def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre| 
+            genre_count[genre] ||= 0
+            genre_count[genre] += 1       
+        end
+        genre_count
+        # hash = {
+        #     @@genres.uniq => @@count
+        # }
+        # (@@genres, freq).histogram
+    end
+
+    def self.artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+            artist_count[artist] ||= 0
+            artist_count[artist] += 1
+        end
+        artist_count
+    end
+    # def self.artist_count
+    #     (@@artists, freq).histogram
+    # end
+end
